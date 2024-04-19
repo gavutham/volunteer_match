7fe384189da69d005ac097f4233b7cbf3b55812c
Gavutham K <kgavutham2005@gmail.com>
2024-04-19 13:23:02 +0530

    create signup form

--- src/pages/auth/signup/SignUp.jsx
+++ src/pages/auth/signup/SignUp.jsx
@@ -1,6 +1,18 @@
-import { useContext, useEffect } from "react";
+import { useContext } from "react";
 import { Context } from "../../../context/context";
 import { useNavigate } from "react-router-dom";
+import {
+  Button,
+  Flex,
+  MultiSelect,
+  PasswordInput,
+  Select,
+  Stack,
+  Text,
+  TextInput,
+} from "@mantine/core";
+import { useForm } from "@mantine/form";
+import { USER } from "../../../utils/constants";
 
 const SignUp = () => {
   const { user, dispatch, isFetching } = useContext(Context);
@@ -12,8 +24,8 @@ const SignUp = () => {
   //   if (user !== null) navigate("/");
   // }, [user]);
 
-  const handleLogin = (e) => {
-    e.preventDefault();
+  const handleSignUp = (values) => {
+    console.log(values);
 
     dispatch({ type: "LOGIN_START" });
     try {
@@ -33,13 +45,81 @@ const SignUp = () => {
       dispatch({ type: "LOGIN_FAILURE" });
     }
   };
+
+  const form = useForm({
+    initialValues: {
+      name: "",
+      email: "",
+      password: "",
+      role: null,
+      tags: [],
+    },
+    validate: {
+      name: (value) => !value && "Name is required",
+      email: (value) =>
+        !value
+          ? "Email is required"
+          : !/\S+@\S+\.\S+/.test(value) && "Invalid email address",
+      password: (value) => !value && "Password is required",
+      role: (value) => !value && "Role is required",
+    },
+  });
+
   return (
-    <div>
-      {/* build the signup form here*/}
-      <button disabled={isFetching} onClick={handleLogin}>
-        signup
-      </button>
-    </div>
+    <Flex align="center" justify="center" h={"100vh"}>
+      <form onSubmit={form.onSubmit((values) => handleSignUp(values))}>
+        <Stack gap="md" w={500} justify="center">
+          <Flex justify="center" gap="xs" align="flex-end">
+            <Text fw={500} size="lg">
+              Sign Up
+            </Text>
+            <Text fw={200} size="sm">
+              into
+            </Text>
+          </Flex>
+          <Text
+            size="xl"
+            fw={700}
+            style={{ textAlign: "center", fontSize: 32 }}
+          >
+            Volunteer Match
+          </Text>
+          <TextInput
+            label="Name"
+            placeholder="Enter your Name"
+            {...form.getInputProps("name")}
+          />
+          <TextInput
+            label="Email"
+            placeholder="Enter your Email"
+            {...form.getInputProps("email")}
+          />
+          <PasswordInput
+            label="Password"
+            placeholder="Enter your Password"
+            {...form.getInputProps("password")}
+          />
+          <Select
+            label="Role"
+            placeholder="Select Your Role"
+            data={USER.ROLES}
+            onChange={(value) => setRole(value)}
+            {...form.getInputProps("role")}
+          />
+          {form.values.role === "Volunteer" && (
+            <MultiSelect
+              label="Interests"
+              placeholder="Select relevant Tags"
+              data={USER.TAGS}
+              {...form.getInputProps("tags")}
+            />
+          )}
+          <Button type="submit" loading={isFetching}>
+            Sign Me Up
+          </Button>
+        </Stack>
+      </form>
+    </Flex>
   );
 };
 
---
+++ src/utils/constants.js
@@ -0,0 +1,18 @@
+export const USER = {
+  ROLES: ["Volunteer", "Organizer"],
+  TAGS: [
+    "Community Service",
+    "Charity",
+    "Fundraising",
+    "Social Impact",
+    "Philanthropy",
+    "Nonprofit",
+    "Civic Engagement",
+    "Environmental Conservation",
+    "Homelessness",
+    "Youth Empowerment",
+    "Health and Wellness",
+    "Education",
+    "Animal Welfare",
+  ],
+};
