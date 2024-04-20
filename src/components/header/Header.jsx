fafea90fd411b4c01ac5fc2d85784eddd1fd1889
Gavutham K <kgavutham2005@gmail.com>
2024-04-20 13:59:02 +0530

    cleanup

--- src/components/header/Header.jsx
+++ src/components/header/Header.jsx
@@ -1,51 +1,29 @@
-import { useState } from "react";
-import {
-  Container,
-  Group,
-  Burger,
-  Text,
-  Avatar,
-  Menu,
-  UnstyledButton,
-} from "@mantine/core";
-import { useDisclosure } from "@mantine/hooks";
+import { Group, Text, Avatar, Box } from "@mantine/core";
 import classes from "./Header.module.css";
-import { useContext, useEffect } from "react";
+import { useContext } from "react";
 import { Context } from "../../context/context.jsx";
-
-// const user = {
-//   name: "Jane Spoonfighter",
-//   email: "janspoon@fighter.dev",
-//   image:
-//     "https://raw.githubusercontent.com/mantinedev/mantine/master/.demo/avatars/avatar-5.png",
-// };
+import { useNavigate } from "react-router-dom";
 
 function Header() {
   const { user, dispatch } = useContext(Context);
+  const navigate = useNavigate();
+
   const links = [
-    { link: "/login", label: "Logout" },
-    { link: "/profile", label: user.name, image: user.image },
+    { label: "Logout" },
+    { link: "/leaderboard", label: "Leaderboard" },
   ];
-  const [opened, { toggle }] = useDisclosure(false);
-  const [active, setActive] = useState(links[0].link);
 
   const items = links.map((link) => (
     <a
       key={link.label}
-      href={link.link}
       className={classes.link}
-      //   data-active={active === link.link || undefined}
       onClick={(event) => {
         event.preventDefault();
-        setActive(link.link);
-        console.log("Clicked:", link.label);
 
         if (link.label === "Logout") {
-          // Redirect to the login page
           dispatch({ type: "LOGOUT" });
-        }
-        if (link.label === user.name) {
-          // Redirect to the login page
+        } else {
+          navigate(link.link);
         }
       }}
     >
@@ -56,20 +34,23 @@ function Header() {
   return (
     <div>
       <header className={classes.header}>
-        <Container size="md" className={classes.inner}>
+        <Box className={classes.inner}>
           <Text size="25px">Volunteer Match</Text>
           <Group gap={5} visibleFrom="xs">
             {items}
-            {/* if items.link.label == user: */}
-            <Group gap={7}>
-              <Avatar src={user.image} alt={user.name} radius="xl" size={20} />
-              <Text fw={500} size="sm" lh={1} mr={3}>
-                {user.name}
+            <Group
+              gap="sm"
+              className={classes.link}
+              style={{ border: "1px solid #724804", borderRadius: "5px" }}
+              onClick={() => navigate("/profile")}
+            >
+              <Text fw={500} size="sm">
+                {user?.name}
               </Text>
+              <Avatar src="/user.png" alt={user?.name} radius="xl" size={20} />
             </Group>
           </Group>
-          {/* <Burger opened={opened} onClick={toggle} hiddenFrom="xs" size="sm" /> */}
-        </Container>
+        </Box>
       </header>
     </div>
   );
