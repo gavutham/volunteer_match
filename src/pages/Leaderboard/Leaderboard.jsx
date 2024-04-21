de5d23b94655a72da9eb81ff0beea1444a5ca609
Gavutham K <kgavutham2005@gmail.com>
2024-04-21 22:21:21 +0530

    create basic components for leaderboard page

--- src/pages/Leaderboard/Leaderboard.jsx
+++ src/pages/Leaderboard/Leaderboard.jsx
@@ -1,5 +1,77 @@
+import { Divider, Flex, Image, Text } from "@mantine/core";
+import Header from "../../components/header/Header";
+import classes from "./Leaderboard.module.css";
+import LeaderBoardTable from "../../components/LeaderBoardTable/LeaderBoardTable";
+
+const alltime = [
+  {
+    name: "User 1",
+    points: 50,
+  },
+  {
+    name: "User 1",
+    points: 50,
+  },
+  {
+    name: "User 1",
+    points: 50,
+  },
+  {
+    name: "User 1",
+    points: 50,
+  },
+  {
+    name: "User 1",
+    points: 50,
+  },
+  {
+    name: "User 1",
+    points: 50,
+    position: 32,
+  },
+];
+
 const Leaderboard = () => {
-  return <h1>Leaderboard</h1>;
+  return (
+    <div>
+      <Header />
+      <Flex h={"100%"}>
+        <div className={classes.imgContainer}>
+          <Image src="/celeb.jpg" mah="100%" />
+        </div>
+        <div className={classes.wrapper}>
+          <Text fw={700} fz="36px" m="xl" style={{ letterSpacing: 2 }}>
+            LeaderBoard
+          </Text>
+
+          <div className={classes.container}>
+            <Text fw={500} fz="30px">
+              All Time
+            </Text>
+            <LeaderBoardTable list={alltime} />
+            <Divider w="40%" my="20px" />
+          </div>
+          <div className={classes.container}>
+            <Text fw={500} fz="30px">
+              Monthly
+            </Text>
+            <LeaderBoardTable list={alltime} />
+            <Divider w="40%" my="20px" />
+          </div>
+          <div className={classes.container}>
+            <Text fw={500} fz="30px">
+              Weekly
+            </Text>
+            <LeaderBoardTable list={alltime} />
+            <Divider w="40%" my="20px" />
+          </div>
+        </div>
+        <div className={classes.imgContainer}>
+          <Image src="/celeb-r.jpg" mah="100%" />
+        </div>
+      </Flex>
+    </div>
+  );
 };
 
 export default Leaderboard;
