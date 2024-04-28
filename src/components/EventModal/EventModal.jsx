68abf26a04a42ef7e88a24666722add996f8cfb9
Aswathy05 <aswathyme1@gmail.com>
2024-04-28 15:20:38 +0530

    event modal updated

--- src/components/EventModal/EventModal.jsx
+++ src/components/EventModal/EventModal.jsx
@@ -10,41 +10,67 @@ import {
 } from "@mantine/core";
 import { useDisclosure } from "@mantine/hooks";
 import {
-  IconCalendar,
+  IconCalendarFilled,
   IconMapPinFilled,
   IconUserFilled,
+  IconCoins,
+  IconCircleCheckFilled,
 } from "@tabler/icons-react";
 import React, { useState } from "react";
+import classes from "./EventModal.module.css";
+import Events from "../Events/Events";
+import { render } from "react-dom";
 
-const EventModal = ({ event, title, location, date }) => {
+const EventModal = ({ event, organizer }) => {
+  const opted_len = event.opted.length;
+  console.log(event);
   return (
-    <div>
+    <div className={classes.card}>
       <Card padding="lg" radius="md" w={"100%"}>
         <Card.Section>
           <Image src="/public/events.png" height={160} alt="event-img" />
         </Card.Section>
-        <Group justify="space-between" mt="md" mb="0">
+
+        <Group justify="space-between" mt="md">
           <Text fw={500} fz={"28px"}>
             {event.title}
           </Text>
-          <Group justify="left" gap={"2px"}>
-            <IconCalendar stroke={1} size={20} />
-            <Text size="xs" c="dimmed">
-              {event.time.toLocaleString()}
+          <Group gap={"2px"} mt="xs">
+            <IconCalendarFilled stroke={1} size={20} />
+            <Text size="xs">{event.time.toLocaleString()}</Text>
+          </Group>
+        </Group>
+
+        <Group justify="space-between" mb="xs">
+          <Group>
+            <Text size="sm" c="dimmed">
+              by
+            </Text>
+            <Text size="md" fw={400}>
+              {organizer?.name}
             </Text>
           </Group>
         </Group>
-        <Group justify="right" gap={"2px"}>
-          <IconMapPinFilled stroke={2} size={15} color="red" />
-          <Text size="xs" c="">
-            {event.location}
-          </Text>
-        </Group>
-        <Group justify="right" gap={"2px"}>
-          <IconUserFilled stroke={2} size={15} />
-          <Text size="xs" c="" gap={"2px"}>
-            limit: {event.limit}
-          </Text>
+
+        <Group justify="left">
+          <Group justify="right" gap={"2px"}>
+            <IconMapPinFilled stroke={1} size={20} fw={500} />
+            <Text size="xs" fw={500}>
+              {event.location}
+            </Text>
+          </Group>
+          <Group justify="right" gap={"2px"}>
+            <IconUserFilled stroke={1} size={20} fw={500} />
+            <Text size="xs" c="" gap={"2px"} fw={500}>
+              Limit: {event.limit}
+            </Text>
+          </Group>
+          <Group justify="right" gap={"2px"}>
+            <IconCoins stroke={1} size={20} fw={500} />
+            <Text size="xs" fw={500}>
+              Points: {event.points}
+            </Text>
+          </Group>
         </Group>
 
         <Flex gap="sm" my="md" wrap="wrap">
@@ -55,6 +81,16 @@ const EventModal = ({ event, title, location, date }) => {
           ))}
         </Flex>
         <Text size="sm">{event.desc}</Text>
+        <Button type="submit" mt="sm" disabled={opted_len > event.limit}>
+          Opt me
+        </Button>
+
+        <Group justify="right" mt="md" gap={"2px"}>
+          <IconCircleCheckFilled stroke={1} size={20} fw={500} />
+          <Text size="xs" fw={400}>
+            Opted: {opted_len}
+          </Text>
+        </Group>
       </Card>
     </div>
   );
