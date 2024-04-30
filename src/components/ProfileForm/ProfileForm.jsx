707b76e8f324f7f4d2c2b5b7b7f6b84ea2fb1fc2
Gavutham K <kgavutham2005@gmail.com>
2024-04-30 20:01:03 +0530

    add header and integrate api in update profile form

---
+++ src/components/ProfileForm/ProfileForm.jsx
@@ -0,0 +1,107 @@
+import { useContext, useState } from "react";
+import {
+  Button,
+  Flex,
+  MultiSelect,
+  Stack,
+  Text,
+  TextInput,
+} from "@mantine/core";
+import { useForm } from "@mantine/form";
+import { Context } from "../../context/context";
+import { USER } from "../../utils/constants";
+import api from "../../services/api";
+
+const ProfileForm = () => {
+  const { user, dispatch, isFetching } = useContext(Context);
+  const [error, setError] = useState(false);
+  const [loading, setLoading] = useState(false);
+
+  const handleUpdate = async (values) => {
+    setError(false);
+    setLoading(true);
+
+    const updatedUser = {
+      ...user,
+      email: values.email,
+      name: values.name,
+      password: values.password,
+      tags: values.tags,
+    };
+
+    const path = `/user/${user._id}`;
+
+    try {
+      const res = await api.put(path, updatedUser);
+
+      if (res.status === 200) {
+        dispatch({ type: "LOGIN_SUCCESS", payload: res.data });
+        setLoading(false);
+      } else {
+        setError(true);
+        console.log(res);
+      }
+    } catch (error) {
+      setError(true);
+      console.log(error);
+    }
+  };
+
+  const form = useForm({
+    initialValues: {
+      name: user.name,
+      email: user.email,
+      password: user.password,
+      tags: user.tags,
+    },
+    validate: {
+      name: (value) => !value && "Enter a valid name",
+      password: (value) => !value && "Enter a valid password",
+    },
+  });
+
+  return (
+    <Flex w={"60%"} align="center" justify="center">
+      <form onSubmit={form.onSubmit((values) => handleUpdate(values))}>
+        <Stack gap="md" w={500} justify="center">
+          <Text fw={700} size="lg" style={{ alignSelf: "start", fontSize: 30 }}>
+            User Profile
+          </Text>
+          <TextInput
+            label="Name"
+            placeholder="Enter your Name"
+            {...form.getInputProps("name")}
+          />
+          <TextInput
+            label="Email"
+            placeholder="Enter your Email"
+            {...form.getInputProps("email")}
+          />
+          <TextInput
+            label="Password"
+            placeholder="Enter your Password"
+            {...form.getInputProps("password")}
+          />
+          {user.role === "Volunteer" && (
+            <MultiSelect
+              label="Interests"
+              placeholder="Select relevant Tags"
+              data={USER.TAGS}
+              {...form.getInputProps("tags")}
+            />
+          )}
+          <Button type="submit" loading={isFetching || loading} color="#135D66">
+            Submit
+          </Button>
+          {error && (
+            <Text size="md" c="red" align="center">
+              Some Error Occurred, Please Try Again
+            </Text>
+          )}
+        </Stack>
+      </form>
+    </Flex>
+  );
+};
+
+export default ProfileForm;
--- src/pages/profile/Profile.jsx
+++ src/pages/profile/Profile.jsx
@@ -14,94 +14,45 @@ import {
 import { useForm } from "@mantine/form";
 import { Context } from "../../context/context";
 import { USER } from "../../utils/constants";
-
+import ProfileForm from "../../components/ProfileForm/ProfileForm";
+import Header from "../../components/header/Header";
 
 const Profile = () => {
   const { user, dispatch, isFetching } = useContext(Context);
   const [error, setError] = useState(false);
   const navigate = useNavigate();
 
-  const handleSignUp = async (values) => {
-    
-  };
-
-  const form = useForm({
-    initialValues: {
-      name: user.name,
-      email: user.email,
-      password: user.password,
-      tags: [],
-    },
-    validate: {
-      name: (value) => !value && "Enter a valid name",
-      password: (value) => !value && "Enter a valid password",
-      
-    },
-  });
-
-  console.log(error);
-
   return (
-    <Flex align="center" justify="center" h={"100vh"} w={"100%"}>
-  <Flex w={"60%"} align="center" justify="center">
-    <form onSubmit={form.onSubmit((values) => handleSignUp(values))}>
-      <Stack gap="md" w={500} justify="center">
-        <Flex justify="center" gap="xs" align="flex-end">
-          <Text fw={500} size="lg" style={{ textAlign: "center", fontSize: 30}}>
-            PROFILE
-          </Text>
+    <>
+      <Header />
+      <Flex align="center" justify="center" h={"calc(100vh - 75px)"} w={"100%"}>
+        <ProfileForm />
+        <Flex w={"40%"} align="center" justify="center">
+          <Stack diredction="column" gap="md" justify="center">
+            <div
+              style={{
+                height: "40%",
+                borderRadius: "50%",
+                overflow: "hidden",
+                width: "70%",
+              }}
+            >
+              <img
+                src="../../../public/user.jpg"
+                alt="Current user's profile picture"
+                style={{ width: "100%", height: "100%", objectFit: "cover" }}
+              />
+            </div>
+            <Flex justify="center" style={{ width: "70%" }}>
+              <Button style={{ marginTop: "10px" }} color="#135D66">
+                Choose Profile Picture
+              </Button>
+            </Flex>
+          </Stack>
         </Flex>
-        <Text
-          size="xl"
-          fw={700}
-          style={{ textAlign: "center", fontSize: 32 }}
-        >
-          Volunteer Match
-        </Text>
-        <TextInput
-          label="Name"
-          placeholder="Enter your Name"
-          {...form.getInputProps("name")}
-        />
-        <TextInput
-          label="Email"
-          placeholder="Enter your Email"
-          {...form.getInputProps("email")}
-        />
-        <PasswordInput
-          label="Password"
-          placeholder="Enter your Password"
-          {...form.getInputProps("password")}
-        />
-        <Button type="submit" loading={isFetching}>
-          Submit
-        </Button>
-        {error && (
-          <Text size="md" c="red" align="center">
-            Some Error Occurred, Please Try Again
-          </Text>
-        )}
-      </Stack>
-    </form>
-  </Flex>
-  <Flex w={"40%"} align="center" justify="center">
-    <Stack diredction="column" gap="md" justify="center">
-      <div style={{ height: "40%", borderRadius: "50%", overflow: "hidden", width: "70%" }}>
-        <img
-          src="../../../public/user.jpg"
-          alt="Current user's profile picture"
-          style={{ width: "100%", height: "100%", objectFit: "cover" }}
-        />
-      </div>
-      <Flex justify="center" style={{width:"70%"}}>
-        <Button style={{marginTop:"10px"}} >Change the profile</Button>
       </Flex>
-    </Stack>
-  </Flex>
-</Flex>
-
+    </>
   );
-
 };
 
 export default Profile;
